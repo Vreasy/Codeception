f5a53bd858402fec3848e1a27e312b0a60843ef7
Mauro Asprea <mauroasprea@gmail.com>
2017-04-10 13:36:25 +0200

    Fix ZF1 not resetting sessions correctly
    From #3a78ed00c

--- src/Codeception/Module/ZF1.php
+++ src/Codeception/Module/ZF1.php
@@ -161,7 +161,7 @@ class ZF1 extends Framework
         }
         \Zend_Layout::resetMvcInstance();
         \Zend_Controller_Action_HelperBroker::resetHelpers();
-        \Zend_Session::$_unitTestEnabled = true;
+        \Zend_Session::$_unitTestEnabled = false;
         \Zend_Registry::_unsetInstance();
         $this->queries = 0;
         $this->time = 0;
